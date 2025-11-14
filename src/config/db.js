d85cb2d63650df12a7932a05dbd0a8e303b6fe72
GuilhermeLuis242 <guilherme.rodrigues@ecenture.com.br>
2025-11-14 20:34:33 -0300

    Changes mongodb connections

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,25 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
+import { execSync } from 'child_process';
 import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
 export const prisma = new PrismaClient();
-
+const schemaPath = path.resolve("prisma/schema.prisma");
 
 export const connectDB = async () => {
     const { DB_URL, NODE_ENV } = process.env;
     try {
         if (!DB_URL) {
             throw new Error('Database is not defined in environment variables');
+        }
+        if (fs.existsSync(schemaPath)) {
+            execSync(`npx prisma generate --schema=${schemaPath}`);
+            if (NODE_ENV !== 'production') {
+                execSync(`npx prisma db push --schema=${schemaPath}`);
+            }
         }
         const connect = await mongoose.connect(DB_URL);
         console.log(`MongoDB Connected: ${connect.connection.host}`);
